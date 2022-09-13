7aab421aeff1f7ecf95ab715b2b87055b79f28c1
Jagan Challa <Jagan_Challa@carmax.com>
2022-09-12 22:56:13 -0700

    refactor: BooksAPI file moved to utils parent -> child props flow working added route to files

--- starter/src/components/BookList.js
+++ starter/src/components/BookList.js
@@ -2,13 +2,17 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Book from "./Book.js";
 
-const BookList = ({books}) => {
-    const book = {};
-    return (
-        <div>
-            <Book book={book}/>
-        </div>
-    );
+const BookList = ({ books }) => {
+  const book = {};
+  return (
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map((book) => {
+          return <Book book={book} key={book.id} />;
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default BookList;
--- starter/src/components/Search.js
+++ starter/src/components/Search.js
@@ -1,29 +1,32 @@
 
+/**
+ * @description function object for Search component
+ * @returns jsx for search page
+ */
 const Search = () => {
+  return (
+    <div className="app">
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a
+            className="close-search"
 
-    return (
-        <div className="app">
-            <div className="search-books">
-              <div className="search-books-bar">
-                <a
-                  className="close-search"
-                  
-                >
-                  Close
-                </a>
-                <div className="search-books-input-wrapper">
-                  <input
-                    type="text"
-                    placeholder="Search by title, author, or ISBN"
-                  />
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div>
-            </div>
-          );
+          >
+            Close
+          </a>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title, author, or ISBN"
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
